e456bd7e60073c0948d393a1dc1a16714ca6907e
Sulianto1992 <Sulianto1992@gmail.com>
2019-09-08 14:33:06 +0800

    First Hello World Test to Jenkins

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld; //name of the package
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	} //end main
+
+} //end class
